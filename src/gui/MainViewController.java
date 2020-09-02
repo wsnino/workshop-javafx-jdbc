1195419bd07a77b990214d1c083992573233a98e
Marcelo Pereira <wsnino.contato@gmail.com>
2020-09-02 15:02:46 -0300

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -35,6 +35,7 @@ public class MainViewController implements Initializable{
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
+		loadeView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
