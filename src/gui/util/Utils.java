e68a1977ebf94de463e72e878b1a1fb8cbe05b91
Marcelo Pereira <wsnino.contato@gmail.com>
2020-09-12 06:18:13 -0300

    Seller TableView

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,7 +1,6 @@
 package gui.util;
 
 import java.text.SimpleDateFormat;
-
 import java.util.Date;
 import java.util.Locale;
 
@@ -20,7 +19,6 @@ public class Utils {
 	public static Integer tryParseToInt(String str) {
 		try {
 			return Integer.parseInt(str);
-
 		} catch (NumberFormatException e) {
 			return null;
 		}
@@ -42,27 +40,24 @@ public class Utils {
 				}
 			};
 			return cell;
-
 		});
 	}
 
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
 		tableColumn.setCellFactory(column -> {
-		TableCell<T, Double> cell = new TableCell<T, Double>() {
-	
-	@Override
-	protected void updateItem(Double item, boolean empty) {
-		super.updateItem(item, empty);
-		if (empty) {
-			setText(null);
-		} else {
-			Locale.setDefault(Locale.US);
-			setText(String.format("%." + decimalPlaces + "f", item));
-		}
-	  }	
-	};
-	return cell;
-	});
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
-  		
 }
